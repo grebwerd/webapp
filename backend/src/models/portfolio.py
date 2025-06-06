0c8fea069d99632f06bbbe754f3e663d4ab17318
grebwerd <grebwerd@gmail.com>
2025-06-06 00:53:41 -0600

    fixing utc typo

--- backend/src/models/portfolio.py
+++ backend/src/models/portfolio.py
@@ -11,6 +11,6 @@ class Portfolio(SQLModel, table=True):
     id: int  = Field(primary_key=True)
     user: str = Field(nullable=False)
     name: str = Field(nullable=False)
-    created_at: datetime = Field(nullable=False, default_factory=datetime.utcnow())
+    created_at: datetime = Field(nullable=False, default_factory=datetime.utcnow)
     updated_at: datetime = Field(nullable=True)
     deleted_at: datetime = Field(nullable=True)
